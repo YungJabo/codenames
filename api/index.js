ccacf804c8795949e1241bedb656ae7e3a008ca4
Максим Кирсанов <kirsmaxi@gmail.com>
2024-08-14 17:28:07 +0300

    vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { setupWebSocket } from "./ws.js";
+import { setupWebSocket } from "../ws.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-const buildPath = path.join(__dirname, "dist");
+const buildPath = path.join(__dirname, "../dist");
 app.use(express.static(buildPath));
 // Запуск сервера
 const server = app.listen(PORT, () => {
